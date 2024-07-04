78b49560ffb9064ad00f090f73d1879213578934
0ghny <6640409+0ghny@users.noreply.github.com>
2024-07-04 17:27:15 +0200

    fix tests

--- plugins/asdf.sh
+++ plugins/asdf.sh
@@ -5,7 +5,7 @@ fi
 
 ASDF_TARGET_VERSION="${SHELLX_PLUGIN_ASDF_VERSION:-v0.14.0}"
 
-if [[ ! -d "${ASDF_DIR}" ]] && [[ "${SHELLX_PLUGIN_ASDF_SKIP_INSTALL:-true}" != "true" ]]; then
+if [[ ! -d "${ASDF_DIR}" ]]; then
   git clone --quiet https://github.com/asdf-vm/asdf.git "${ASDF_DIR}" --branch "${ASDF_TARGET_VERSION}" > /dev/null 2>&1 || \
   echo "shellx-plugin-asdf: error cloning asdf, skipping initialisation"
 fi
